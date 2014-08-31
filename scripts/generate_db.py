defba371033adf64aef1f058c0137e335b3567ad
Nicolas DUBIEN <github@dubien.org>
2014-08-31 19:36:10 +0100

    'with' keyword to close db

--- scripts/generate_db.py
+++ scripts/generate_db.py
@@ -13,30 +13,30 @@ DEFAULT_DB = "run-tracking.db"
 
 def generate_tables(db=DEFAULT_DB):
     conn = sqlite3.connect(db)
-    c = conn.cursor()
     
-    # Create tables
-    c.execute('''CREATE TABLE runs (
-                    id INTEGER PRIMARY KEY,
-                    start_time TEXT,
-                    time_s INTEGER,
-                    distance_m INTEGER,
-                    calories INTEGER,
-                    creator TEXT)''')
-    c.execute('''CREATE TABLE points (
-                    id INTEGER PRIMARY KEY,
-                    run_id INTEGER,
-                    datetime TEXT,
-                    latitude_d REAL,
-                    longitude_d REAL,
-                    altitude_m REAL,
-                    distance_m INTEGER,
-                    FOREIGN KEY(run_id) REFERENCES runs(id))''')
-    
-    # Commit the changes
-    #   and close the connection
-    conn.commit()
-    conn.close()
+    with conn:
+        c = conn.cursor()
+        
+        # Create tables
+        c.execute('''CREATE TABLE runs (
+                        id INTEGER PRIMARY KEY,
+                        start_time TEXT,
+                        time_s INTEGER,
+                        distance_m INTEGER,
+                        calories INTEGER,
+                        creator TEXT)''')
+        c.execute('''CREATE TABLE points (
+                        id INTEGER PRIMARY KEY,
+                        run_id INTEGER,
+                        datetime TEXT,
+                        latitude_d REAL,
+                        longitude_d REAL,
+                        altitude_m REAL,
+                        distance_m INTEGER,
+                        FOREIGN KEY(run_id) REFERENCES runs(id))''')
+        
+        # Commit the changes
+        conn.commit()
 
 if __name__ == '__main__':
     generate_tables(DEFAULT_DB)
