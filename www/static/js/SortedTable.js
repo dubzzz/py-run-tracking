7e9034cd893e86d9d17f8a95221c4726df58ed04
Nicolas DUBIEN <github@dubien.org>
2014-09-02 03:00:01 +0100

    Better format in SortedTable for distance/time/speed

--- www/static/js/SortedTable.js
+++ www/static/js/SortedTable.js
@@ -1,13 +1,38 @@
 // Custom sort method
-Array.prototype.sortOnColumn = function(column, data_type){
+Array.prototype.sortOnColumn = function(column, data_type) {
 	this.sort(function(a, b){
-			if(a[column] < b[column]){
+		if(a[column] < b[column]){
 			return -1;
-			} else if(a[column] > b[column]) {
+		} else if(a[column] > b[column]) {
 			return 1;
-			}
-			return 0;
-			});
+		}
+		return 0;
+	});
+}
+
+function formatSortedTableData(rawdata, content) {
+	if (content == "time") { // in seconds
+		var hours = Math.floor(rawdata/3600);
+		rawdata -= 3600 * hours;
+		var minutes = Math.floor(rawdata/60);
+		rawdata -= 60 * minutes;
+		var minutes_txt = minutes < 10 ? "0"+minutes : minutes.toString();
+		var seconds = parseInt(rawdata);
+		var seconds_txt = seconds < 10 ? "0"+seconds : seconds.toString();
+		return hours + ":" + minutes_txt + ":" + seconds_txt;
+	} else if (content == "distance" || content == "speed") {
+		var distance_km = rawdata/1000.;
+		var distance_remaining_100m = parseInt(10*distance_km)%10;
+		if (content == "distance")
+			return Math.floor(distance_km) + ","
+					+ distance_remaining_100m + " km";
+		else if (content == "distance")
+			return Math.floor(distance_km) + ","
+					+ distance_remaining_100m + " km/h";
+		
+		return Math.floor(distance_km) + "," + distance_remaining_100m;
+	} else
+		return rawdata;
 }
 
 // Contains all the SortedTable that have been initialized
@@ -22,6 +47,8 @@ function SortedTable(table, rawdata_details, rawdata) {
 	//  type: Specify which kind of data is available in each column
 	//        types are: int, text, date, time..
 	//  unit: show a particular unit
+	//  content: apply custom display depending on the content
+	//        contents are: datetime, time, distance
 	this.rawdata_details = rawdata_details;
 
 	// The data that should be put in the table 
@@ -68,10 +95,16 @@ function SortedTable(table, rawdata_details, rawdata) {
 			var tr = $("<tr/>");
 			for (var j=0 ; j!=this.rawdata_details.length +1 ; j++) {
 				var td = $("<td/>");
-				if (j > 0 && "unit" in this.rawdata_details[j-1])
-					td.html(this.rawdata[i][j] + " "
-							+ this.rawdata_details[j-1]["unit"]);
-				else
+				if (j > 0) {
+					if ("unit" in this.rawdata_details[j-1])
+						td.html(this.rawdata[i][j] + " "
+								+ this.rawdata_details[j-1]["unit"]);
+					else if ("content" in this.rawdata_details[j-1])
+						td.html(formatSortedTableData(this.rawdata[i][j],
+								this.rawdata_details[j-1]["content"]));
+					else
+						td.html(this.rawdata[i][j]);
+				} else
 					td.html(this.rawdata[i][j]);
 				tr.append(td);
 			}
