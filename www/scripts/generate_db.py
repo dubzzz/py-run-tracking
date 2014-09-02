74c66eb547022d01cc0855ac709c3ff9e72aae9c
Nicolas DUBIEN <github@dubien.org>
2014-09-02 18:03:56 +0100

    Create tables for sections

--- www/scripts/generate_db.py
+++ www/scripts/generate_db.py
@@ -22,18 +22,20 @@ def generate_tables(db=DEFAULT_DB):
         c = conn.cursor()
         
         # Drop tables if they exist
-        c.execute('''DROP TABLE IF EXISTS runs''')
-        c.execute('''DROP TABLE IF EXISTS points''')
+        #c.execute('''DROP TABLE IF EXISTS runs''')
+        #c.execute('''DROP TABLE IF EXISTS points''')
+        #c.execute('''DROP TABLE IF EXISTS sections''')
+        #c.execute('''DROP TABLE IF EXISTS section_run''')
         
         # Create tables
-        c.execute('''CREATE TABLE runs (
+        c.execute('''CREATE TABLE IF NOT EXISTS runs (
                         id INTEGER PRIMARY KEY,
                         start_time TEXT,
                         time_s INTEGER,
                         distance_m INTEGER,
                         calories INTEGER,
                         creator TEXT)''')
-        c.execute('''CREATE TABLE points (
+        c.execute('''CREATE TABLE IF NOT EXISTS points (
                         id INTEGER PRIMARY KEY,
                         run_id INTEGER,
                         datetime TEXT,
@@ -42,6 +44,19 @@ def generate_tables(db=DEFAULT_DB):
                         altitude_m REAL,
                         distance_m INTEGER,
                         FOREIGN KEY(run_id) REFERENCES runs(id))''')
+        c.execute('''CREATE TABLE IF NOT EXISTS sections (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS section_run (
+                        id INTEGER PRIMARY KEY,
+                        section_id INTEGER,
+                        run_id INTEGER,
+                        from_id INTEGER,
+                        to_id INTEGER,
+                        FOREIGN KEY(section_id) REFERENCES sections(id),
+                        FOREIGN KEY(run_id) REFERENCES runs(id),
+                        FOREIGN KEY(from_id) REFERENCES points(id),
+                        FOREIGN KEY(to_id) REFERENCES points(id))''')
         
         # Commit the changes
         conn.commit()
