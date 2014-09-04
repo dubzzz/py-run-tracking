478ea09860262ba6bef19c34ed07fc40ae608009
Nicolas DUBIEN <github@dubien.org>
2014-09-05 00:04:58 +0100

    Add funny values on items (number of burgers..)

--- www/static/js/SortedTable.js
+++ www/static/js/SortedTable.js
@@ -13,7 +13,16 @@ Array.prototype.sortOnColumn = function(column, reversed) {
 function numberToString(number, num_decimals) {
 	var power = Math.pow(10, num_decimals);
 	var number_bis = Math.round(number * power);
-	return Math.floor(number_bis/power) + "," + (number_bis%power);
+	
+	var returned_txt = Math.floor(number_bis/power) + ",";
+	
+	var decimals = number_bis%power;
+	var missing_decimals = num_decimals
+			- (decimals == 0 ? 1 : Math.ceil(Math.log10(decimals+1)));
+	for (var i=0 ; i!= missing_decimals ; i++) {
+		returned_txt += "0";
+	}
+	return returned_txt + decimals;
 }
 
 function formatSortedTableData(rawdata, content) {
@@ -81,9 +90,86 @@ function formatSortedTableData(rawdata, content) {
 		return rawdata;
 }
 
+var known_content_format = [
+		"time", "distance", "speed", "date", "datetime",
+		"thousands", "url", "html",
+];
+var known_content_funny_values = {
+		calories: [
+			{value: 12.025, name: "M&M's",},
+			{value: 180, name: "Croissant", plural: "Croissants"},
+			{value: 509, name: "Big Mac"},
+		],
+		distance: [
+			{value: 346, name: "Tour de stade de football", plural: "Tours de stade de football"},
+			{value: 50000, name: "Lyon - St Etienne"},
+			{value: 394000, name: "Paris - Lyon"},
+		],
+};	
+
 // Contains all the SortedTable that have been initialized
 // by calling init()
 var list_sortedtables = [];
+
+function addTooltipIfPossible(jquery_element, value, content) {
+	// Unknown content for funny values
+	if (!(content in known_content_funny_values))
+		return;
+	
+	var funny_txt = "";
+	var funny_details = known_content_funny_values[content];
+	for (var i=0 ; i!=funny_details.length ; i++) {
+		var num_funny = value/funny_details[i]['value'];
+		if (num_funny >= 0.005) {
+			funny_txt += '<tr>';
+			funny_txt += '<td>' + numberToString(num_funny, 2) + '</td>';
+			funny_txt += '<td>'
+				+ ((num_funny > 1 && 'plural' in funny_details[i])
+					? funny_details[i]['plural']
+					: funny_details[i]['name'])
+				+ '</td>';
+			funny_txt += '</tr>';
+		}
+	}
+	if (funny_txt.length != 0) {
+		funny_txt = '<table>' + funny_txt + '</table>';
+		jquery_element.tooltip({content: funny_txt, items: "*",
+				show: false, track: true});
+	}
+}
+
+function getFormattedTD(rawdata, rawdata_details) {
+	var td = $("<td/>");
+	// If we have raw details
+	if (rawdata_details != null) {
+		// If a known content
+		if ("content" in rawdata_details
+				&& $.inArray(rawdata_details["content"], known_content_format) != -1) {
+			td.html(formatSortedTableData(rawdata, rawdata_details["content"]));
+		// No known content but a specified unit
+		} else if ("unit" in rawdata_details) {
+			if (rawdata_details["type"] == "int") {
+				td.html(formatSortedTableData(rawdata, "thousands")
+						+ " " + rawdata_details["unit"]);
+			} else {
+				td.html(rawdata + " " + rawdata_details["unit"]);
+			}
+		// No known content, no unit but an int
+		} else if (rawdata_details["type"] == "int") {
+			td.html(formatSortedTableData(rawdata, "thousands"));
+		// Just a text
+		} else {
+			td.html(rawdata);
+		}
+		
+		// Try to find if it can have a tooltip
+		/*if ("content" in rawdata_details) {
+			addTooltipIfPossible(td, rawdata, rawdata_details["content"]);
+		}*/
+	} else
+		td.html(formatSortedTableData(rawdata, "thousands"));
+	return td;
+}
 
 function SortedTable(table, rawdata_details, rawdata) {
 	// HTML element to sort
@@ -165,27 +251,8 @@ function SortedTable(table, rawdata_details, rawdata) {
 		for (var i=0 ; i!=this.rawdata.length ; i++) {
 			var tr = $("<tr/>");
 			for (var j=0 ; j!=this.rawdata_details.length +1 ; j++) {
-				var td = $("<td/>");
-				if (j > 0) {
-					if ("unit" in this.rawdata_details[j-1]) {
-						if (this.rawdata_details[j-1]["type"] == "int")
-							td.html(formatSortedTableData(this.rawdata[i][j],
-									"thousands") + " "
-									+ this.rawdata_details[j-1]["unit"]);
-						else
-							td.html(this.rawdata[i][j] + " "
-									+ this.rawdata_details[j-1]["unit"]);
-					} else if ("content" in this.rawdata_details[j-1])
-						td.html(formatSortedTableData(this.rawdata[i][j],
-								this.rawdata_details[j-1]["content"]));
-					else if (this.rawdata_details[j-1]["type"] == "int")
-						td.html(formatSortedTableData(this.rawdata[i][j],
-								"thousands"));
-					else
-						td.html(this.rawdata[i][j]);
-				} else
-					td.html(formatSortedTableData(this.rawdata[i][j],
-							"thousands"));
+				var td = getFormattedTD(this.rawdata[i][j],
+						j>0 ? this.rawdata_details[j-1] : null)
 				tr.append(td);
 			}
 			tbody.append(tr);
