30c8987049729474cfeb2e16c167d058b9c93b5f
Nicolas DUBIEN <github@dubien.org>
2014-09-04 01:09:00 +0100

    Add tables to register best performances

--- www/scripts/generate_db.py
+++ www/scripts/generate_db.py
@@ -27,6 +27,8 @@ def generate_tables(db=DEFAULT_DB):
         #c.execute('''DROP TABLE IF EXISTS sections''')
         #c.execute('''DROP TABLE IF EXISTS section_run''')
         #c.execute('''DROP TABLE IF EXISTS analyse_section_run''')
+        c.execute('''DROP TABLE IF EXISTS best_score_lines''')
+        c.execute('''DROP TABLE IF EXISTS best_scores''')
         
         # Create tables
         c.execute('''CREATE TABLE IF NOT EXISTS runs (
@@ -64,6 +66,29 @@ def generate_tables(db=DEFAULT_DB):
                         run_id INTEGER,
                         FOREIGN KEY(section_id) REFERENCES sections(id),
                         FOREIGN KEY(run_id) REFERENCES runs(id))''')
+        c.execute('''CREATE TABLE IF NOT EXISTS best_scores (
+                        id INTEGER PRIMARY KEY,
+                        is_distance BOOLEAN,
+                        value INTEGER)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS best_score_lines (
+                        id INTEGER PRIMARY KEY,
+                        best_id INTEGER,
+                        run_id INTEGER,
+                        from_f REAL,
+                        to_f REAL,
+                        FOREIGN KEY(best_id) REFERENCES best_scores(id),
+                        FOREIGN KEY(run_id) REFERENCES runs(id))''')
+        best_scores_init = (
+                (1, 0, 720), # Cooper (12min)
+                (2, 0, 3600), # One-hour run
+                (3, 1, 1000), # 1km
+                (4, 1, 5000), # 5km
+                (5, 1, 10000), # 10km
+                (6, 1, 21100), # Half-marathon
+                (7, 1, 42195), # Marathon
+        )
+        c.executemany('''INSERT INTO best_scores (id, is_distance, value)
+                            VALUES (?,?,?)''', best_scores_init)
         
         # Commit the changes
         conn.commit()
